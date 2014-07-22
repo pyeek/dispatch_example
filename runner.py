52b5ea210a2fe161a6b7f83e95b3b1036c6699bc
Andrew Kou <me@pyeek.com>
2014-07-22 11:16:09 -0700

    Added docstrings for runner

--- runner.py
+++ runner.py
@@ -1,4 +1,17 @@
-# main.py
+"""
+    runner.py
+
+    This is the entry point to the example
+
+    Uses werkzeug.wsgi.DispatcherMiddleware to combine the apps, mounting as
+    follows:
+
+        http://localhost/: root
+        http://localhost/app1: app1
+        http://localhost/app2: app2
+
+    Then uses run_simple to serve it on localhost
+"""
 from werkzeug.serving import run_simple
 from werkzeug.wsgi import DispatcherMiddleware
 
@@ -7,6 +20,7 @@ from dispatch_example.app1 import app as app1
 from dispatch_example.app2 import app as app2
 
 if __name__ == '__main__':
+    # DispatcherMiddleware requires a "root" application (named root in this case)
     app = DispatcherMiddleware(root, {
         '/app1': app1,
         '/app2': app2
